c6678e720011f18d937bf1d7886301d255465214
Francisco González <fragonib@gmail.com>
2023-05-10 19:39:01 +0200

    feat: macros if, unless & times

--- lib/macros.ex
+++ lib/macros.ex
@@ -1,5 +1,5 @@
-defmodule MyIf do
-  def myif(condition, clauses) do
+defmodule My do
+  def if_no_macro(condition, clauses) do
     do_clause = Keyword.get(clauses, :do, nil)
     else_clause = Keyword.get(clauses, :else, nil)
 
@@ -12,7 +12,44 @@ defmodule MyIf do
     end
   end
 
-  defmacro macro(code) do
-    IO.inspect(quote do: IO.inspect(unquote(code)))
+  defmacro if(condition, clauses) do
+    do_clause = Keyword.get(clauses, :do, nil)
+    else_clause = Keyword.get(clauses, :else, nil)
+
+    quote do
+      case unquote(condition) do
+        val when val in [false, nil] -> unquote(else_clause)
+        _ -> unquote(do_clause)
+      end
+    end
   end
+
+  defmacro unless(condition, clause) do
+    do_clause = Keyword.get(clause, :do, nil)
+
+    quote do
+      case unquote(condition) do
+        val when val in [false, nil] -> unquote(do_clause)
+        _ -> nil
+      end
+    end
+
+  end
+
+  defmacro times(n, clause) do
+    do_clause = Keyword.get(clause, :do, nil)
+
+      quote do
+        for _ <- 1..unquote(n) do
+          unquote(do_clause)
+        end
+      end
+
+  end
+
+  @spec macro(any) :: any
+  defmacro macro(code) do
+    IO.inspect(quote do: unquote(code))
+  end
+
 end
