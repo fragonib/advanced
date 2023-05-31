3ec3c4f33775d18d7853dd65c5d63a4e14004cb0
Francisco González <fragonib@gmail.com>
2023-05-31 22:52:24 +0200

    refactor: simplify when version

--- lib/behaviours.ex
+++ lib/behaviours.ex
@@ -1,33 +1,38 @@
 defmodule Tracer do
 
-  defmacro deft({:when, _ , [{name, _, args} = definition, condition]}, do: content) do
+  defmacro deft({:when, _ , [{name, _, args}, _condition]} = definition, do: body) do
     quote do
-      Kernel.def unquote(definition) when unquote(condition) do
+      Kernel.def unquote(definition) do
         IO.puts("[Macro Tracer] ==> call: #{Tracer.dump_defn(unquote(name), unquote(args))}")
-        result = unquote(content)
+        result = unquote(body)
         IO.puts("[Macro Tracer] <== result: #{result}")
         result
       end
     end
   end
 
-  defmacro deft({name, _, args} = definition, do: content) do
-    IO.inspect(definition)
+  defmacro deft({name, _, args} = definition, do: body) do
     quote do
       Kernel.def unquote(definition) do
         IO.puts("[Macro Tracer] ==> call: #{Tracer.dump_defn(unquote(name), unquote(args))}")
-        result = unquote(content)
+        result = unquote(body)
         IO.puts("[Macro Tracer] <== result: #{result}")
         result
       end
     end
+  end
+
+  def trace(name, args, body) do
+    IO.puts("[Macro Tracer] ==> call: #{Tracer.dump_defn(name, args)}")
+    result = Code.eval_quoted(body)
+    IO.puts("[Macro Tracer] <== result: #{result}")
+    result
   end
 
   def dump_args(args) do
     args |> Enum.map(&inspect/1) |> Enum.join(", ")
   end
 
-  @spec dump_defn(any, any) :: <<_::16, _::_*8>>
   def dump_defn(name, args) do
     "#{name}(#{dump_args(args)})"
   end
