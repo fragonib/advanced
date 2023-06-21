2de7bcc347cfc90e5fc0a76f7d3180d4c38fe044
Francisco González Ibáñez <fragonib@gmail.com>
2023-06-21 17:16:28 +0200

    refactor: repackage protocols

--- test/protocols/midi_test.exs
+++ test/protocols/midi_test.exs
@@ -2,19 +2,19 @@ defmodule MidiTest do
   use ExUnit.Case
 
   test "enumerable protocol" do
-    midi = Midi.from_file("test/midi/rap.mid")
+    midi = Midi.from_file("test/protocols/rap.mid")
     frames_list = Enum.take(midi, 2)
     assert length(frames_list) == 2
   end
 
   test "collectable protocol" do
-    midi = Midi.from_file("test/midi/rap.mid")
+    midi = Midi.from_file("test/protocols/rap.mid")
     new_midi = Enum.into(midi, %Midi{})
     assert new_midi == midi
   end
 
   test "collectable protocol aggregates frames" do
-    midi = Midi.from_file("test/midi/rap.mid")
+    midi = Midi.from_file("test/protocols/rap.mid")
     midi2 = %Midi{}
     midi2 = Enum.take(midi, 1) |> Enum.into(midi2)
     midi2 = [Enum.at(midi, 3)] |> Enum.into(midi2)
