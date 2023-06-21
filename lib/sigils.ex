eb39fd0b871d71b7630bc8713db3d388661bff2a
Francisco González Ibáñez <fragonib@gmail.com>
2023-06-21 21:24:52 +0200

    doc: improve documentation

--- lib/sigils.ex
+++ lib/sigils.ex
@@ -2,16 +2,6 @@ defmodule LineSigil do
   @doc """
   Implement the `~l` sigil, which takes a string containing
   multiple lines and returns a list of those lines.
-  report erratum • discuss
-  ## Example usage
-  iex> import LineSigil
-  nil
-  iex> ~l\"""
-  ...> one
-  ...> two
-  ...> three
-  ...> \"""
-  ["one","two","three"]
   """
   def sigil_l(lines, _opts) do
     lines |> String.trim_trailing() |> String.split("\n")
@@ -32,6 +22,11 @@ defmodule ColorSigil do
     @color_map[color_space][String.to_atom(color_name)]
   end
 
+  @doc """
+    excludes standard ´c´ sigill provided by default module
+    ## Example usage
+    use ColorSigil
+  """
   defmacro __using__(_opts) do
     quote do
       import Kernel, except: [sigil_c: 2]
